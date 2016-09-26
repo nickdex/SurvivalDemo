4a4cf8f61263fd8699a3c34c98c33b56e47b9cb1
Nikhil <nikwarke@gmail.com>
2016-09-26 17:32:38 +0530

    upto video 5

---
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/InputAxisScrollbar.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.CrossPlatformInput
+{
+    public class InputAxisScrollbar : MonoBehaviour
+    {
+        public string axis;
+
+	    void Update() { }
+
+	    public void HandleInput(float value)
+        {
+            CrossPlatformInputManager.SetAxis(axis, (value*2f) - 1f);
+        }
+    }
+}
